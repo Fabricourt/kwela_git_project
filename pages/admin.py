74d9205900a0821aaac7584ef506d304053a9fda
patricia <pwagiatha3@gmail.com>
2019-02-14 07:01:36 +0300

    new changes

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Photoi, Photoa
 # Register your models here.
+admin.site.register(Photoi)
+admin.site.register(Photoa)
--- pages/views.py
+++ pages/views.py
@@ -4,11 +4,14 @@ from listings.choices import price_choices, lot_size_choices, state_choices
 
 from listings.models import Listing
 from realtors.models import Realtor
+from pages.models import Photoi, Photoa
 
 def index(request):
+    pages = Photoi.objects.order_by('title').filter(is_published=True)[:3]
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
+        'pages': pages,
         'listings': listings,
         'state_choices': state_choices,
         'lot_size_choices': lot_size_choices,
@@ -17,6 +20,7 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
+    pages = Photoa.objects.order_by('title').filter(is_published=True)[:3]
     # Get all realtors
     realtors = Realtor.objects.order_by('-hire_date')
 
@@ -24,6 +28,7 @@ def about(request):
     mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
 
     context = {
+        'pages': pages,
         'realtors': realtors,
         'mvp_realtors': mvp_realtors
     }
