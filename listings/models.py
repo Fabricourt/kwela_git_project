af538e22afd28aca16b1123bb41b51836219bf14
patricia <pwagiatha3@gmail.com>
2019-02-16 10:52:06 +0300

    kwela logo

--- listings/models.py
+++ listings/models.py
@@ -36,7 +36,7 @@ class Listing(models.Model):
 
 class Logo(models.Model):
     title = models.CharField(max_length=10, blank=True, null=True)
-    kwela = models.ImageField(default='kwela.jpg')
+    kwela = models.ImageField(default='default.jpg', upload_to='kwela_pics')
 
     def __str__(self):
         return self.title
