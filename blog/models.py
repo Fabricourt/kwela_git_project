2eed4f172bbf99f4e2ab97c17b1ea49c9eb9f43b
patricia <pwagiatha3@gmail.com>
2019-03-14 11:08:24 +0300

    home migrations error cleaned

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,6 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/', null=True, blank=True)
-    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', null=True, blank=True)
     def __str__(self):
         return self.title
 
