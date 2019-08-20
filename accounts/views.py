33c28fc768c8bfd796e2b8c37c96fced90978d18
Fabricourt <mfalme2030@gmail.com>
2019-08-20 20:25:19 +0300

    profile face

--- accounts/views.py
+++ accounts/views.py
@@ -61,7 +61,7 @@ def login(request):
     if user is not None:
       auth.login(request, user)
       messages.success(request, 'You are now logged in')
-      return redirect('listings')
+      return redirect('profile')
     else:
       messages.error(request, 'Invalid credentials')
       return redirect('accounts/login')
